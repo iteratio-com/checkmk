2026ffad64d81f9d3f77f1ce0fa4657d35b2cdc2
Lars Michelsen <lm@checkmk.com>
2026-02-14 16:20:20 +0100

    Clean up no-untyped-def #832
    Change-Id: I98dd23c525f001efaaf3e5e9d3457bee4f2f9437

--- cmk/plugins/collection/rulesets/apc_ats_output.py
+++ cmk/plugins/collection/rulesets/apc_ats_output.py
@@ -3,8 +3,6 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
-
 
 from cmk.rulesets.v1 import Title
 from cmk.rulesets.v1.form_specs import (
@@ -23,7 +21,7 @@ from cmk.rulesets.v1.form_specs._migrations import (
 from cmk.rulesets.v1.rule_specs import CheckParameters, HostAndItemCondition, Topic
 
 
-def _parameter_valuespec_apc_ats_output():
+def _parameter_valuespec_apc_ats_output() -> Dictionary:
     return Dictionary(
         title=Title("Levels for ATS output parameters"),
         elements={
