848aff27ac456f6be2b5f4c32acc3a8987daf77a
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-02-24 16:10:13 +0100

    Legacy check migration: cisco_ucs_temp_mem I
    Change-Id: Ic747158e87c2fc8e2d224c38b89d92fd67303d7e

--- cmk/legacy_checks/cisco_ucs_temp_mem.py
+++ cmk/legacy_checks/cisco_ucs_temp_mem.py
@@ -3,54 +3,57 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
-
-
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import SNMPTree, StringTable
-from cmk.legacy_includes.temperature import check_temperature
-from cmk.plugins.cisco.lib_ucs import DETECT
-
-check_info = {}
-
 # comNET GmbH, Fabian Binder - 2018-05-30
 
-# .1.3.6.1.4.1.9.9.719.1.30.12.1.2 memory Unit Name
-# .1.3.6.1.4.1.9.9.719.1.30.12.1.6 cucsMemoryUnitEnvStatsTemperature
-
-
-def discover_cisco_ucs_temp_mem(info):
-    for name, _value in info:
-        name = name.split("/")[3]
-        yield name, {}
-
-
-def check_cisco_ucs_temp_mem(item, params, info):
-    for name, value in info:
-        name = name.split("/")[3]
-        if name == item:
-            temp = int(value)
-            return check_temperature(temp, params, "cisco_temp_%s" % item)
-    return None
+from cmk.agent_based.v2 import (
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    get_value_store,
+    Service,
+    SimpleSNMPSection,
+    SNMPTree,
+    StringTable,
+)
+from cmk.plugins.cisco.lib_ucs import DETECT
+from cmk.plugins.lib.temperature import check_temperature, TempParamType
 
 
 def parse_cisco_ucs_temp_mem(string_table: StringTable) -> StringTable:
     return string_table
 
 
-check_info["cisco_ucs_temp_mem"] = LegacyCheckDefinition(
+def discover_cisco_ucs_temp_mem(section: StringTable) -> DiscoveryResult:
+    for name, _value in section:
+        yield Service(item=name.split("/")[3])
+
+
+def check_cisco_ucs_temp_mem(item: str, params: TempParamType, section: StringTable) -> CheckResult:
+    for name, value in section:
+        if name.split("/")[3] == item:
+            yield from check_temperature(
+                int(value),
+                params,
+                unique_name=f"cisco_temp_{item}",
+                value_store=get_value_store(),
+            )
+
+
+snmp_section_cisco_ucs_temp_mem = SimpleSNMPSection(
     name="cisco_ucs_temp_mem",
-    parse_function=parse_cisco_ucs_temp_mem,
     detect=DETECT,
     fetch=SNMPTree(
         base=".1.3.6.1.4.1.9.9.719.1.30.12.1",
         oids=["2", "6"],
     ),
+    parse_function=parse_cisco_ucs_temp_mem,
+)
+
+check_plugin_cisco_ucs_temp_mem = CheckPlugin(
+    name="cisco_ucs_temp_mem",
     service_name="Temperature Mem %s",
     discovery_function=discover_cisco_ucs_temp_mem,
     check_function=check_cisco_ucs_temp_mem,
     check_ruleset_name="temperature",
-    check_default_parameters={
-        "levels": (75.0, 85.0),
-    },
+    check_default_parameters={"levels": (75.0, 85.0)},
 )
