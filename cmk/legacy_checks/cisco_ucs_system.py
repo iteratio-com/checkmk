5f22ea97e61f139a29418c20953d2f7d258ecbe4
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-02-24 14:20:32 +0100

    Legacy check migration: cisco_ucs_system I
    Change-Id: Ifd929ebc28fb44291810bfd6a21014ae081c955d

--- cmk/legacy_checks/cisco_ucs_system.py
+++ cmk/legacy_checks/cisco_ucs_system.py
@@ -3,41 +3,50 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import SNMPTree, StringTable
-from cmk.plugins.cisco.lib_ucs import DETECT, MAP_OPERABILITY
-
-check_info = {}
-
 # comNET GmbH, Fabian Binder - 2018-05-07
 
-# .1.3.6.1.4.1.9.9.719.1.9.35.1.32 cucsComputeRackUnitModel
-# .1.3.6.1.4.1.9.9.719.1.9.35.1.47 cucsComputeRackUnitSerial
-# .1.3.6.1.4.1.9.9.719.1.9.35.1.43 cucsComputeRackUnitOperability
-
-
-def discover_cisco_ucs_system(info: StringTable) -> list[tuple[None, None]]:
-    return [(None, None)]
-
-
-def check_cisco_ucs_system(_no_item: None, _no_params: None, info: StringTable) -> tuple[int, str]:
-    model, serial, status = info[0]
-    state, state_readable = MAP_OPERABILITY.get(status, (3, "Unknown, status code %s" % status))
-    return state, f"Status: {state_readable}, Model: {model}, SN: {serial}"
+from cmk.agent_based.v2 import (
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    Result,
+    Service,
+    SimpleSNMPSection,
+    SNMPTree,
+    State,
+    StringTable,
+)
+from cmk.plugins.cisco.lib_ucs import DETECT, MAP_OPERABILITY
 
 
 def parse_cisco_ucs_system(string_table: StringTable) -> StringTable | None:
     return string_table or None
 
 
-check_info["cisco_ucs_system"] = LegacyCheckDefinition(
+def discover_cisco_ucs_system(section: StringTable) -> DiscoveryResult:
+    yield Service()
+
+
+def check_cisco_ucs_system(section: StringTable) -> CheckResult:
+    model, serial, status = section[0]
+    state, state_readable = MAP_OPERABILITY.get(status, (3, "Unknown, status code %s" % status))
+    yield Result(
+        state=State(state), summary=f"Status: {state_readable}, Model: {model}, SN: {serial}"
+    )
+
+
+snmp_section_cisco_ucs_system = SimpleSNMPSection(
     name="cisco_ucs_system",
-    parse_function=parse_cisco_ucs_system,
     detect=DETECT,
     fetch=SNMPTree(
         base=".1.3.6.1.4.1.9.9.719.1.9.35.1",
         oids=["32", "47", "43"],
     ),
+    parse_function=parse_cisco_ucs_system,
+)
+
+check_plugin_cisco_ucs_system = CheckPlugin(
+    name="cisco_ucs_system",
     service_name="System health",
     discovery_function=discover_cisco_ucs_system,
     check_function=check_cisco_ucs_system,
