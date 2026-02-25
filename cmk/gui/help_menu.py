342363de83ede676c7cd78cfd8ea87315c7d14c7
Charly Laurent <charly.laurent@checkmk.com>
2026-02-25 16:31:03 +0100

    feat: add link analytics manifest
    Add link to product usage analytics manifest in Help menu.
    
    Closes CMK-31673
    
    Change-Id: I1d0c8343c9bbe1475b69399277b0844838ffe7bf

--- cmk/gui/help_menu.py
+++ cmk/gui/help_menu.py
@@ -234,6 +234,14 @@ def default_about_checkmk_entries() -> list[NavItemTopicEntry]:
             sort_index=30,
             icon=DefaultIcon(id=IconNames.checkmk_logo_min),
         ),
+        NavItemTopicEntry(
+            id="product_usage_analytics_manifest",
+            title=_("Product usage analytics manifest"),
+            url="https://analytics.checkmk.com/manifest",
+            target="_blank",
+            sort_index=40,
+            icon=DefaultIcon(id=IconNames.checkmk_logo_min),
+        ),
     ]
 
 
