935242f78bbddd8d4e530666f36907a7ca3d9ffe
Andreas Hasenkopf <andreas.hasenkopf@checkmk.com>
2026-02-25 15:51:25 +0100

    Fetcher trigger: Create crash reports
    When exceptions happen during execution of the trigger,
    the returned Error object hid all details about it.
    
    With this change, a complementary crash report is generated.
    
    Change-Id: I965d3db8d1d372f2f8cbf1fb7d20011092e11c5e

--- tests/unit/cmk/base/test_automations_check_mk.py
+++ tests/unit/cmk/base/test_automations_check_mk.py
@@ -9,6 +9,7 @@
 
 from collections.abc import Mapping, Sequence
 from dataclasses import replace
+from pathlib import Path
 
 import pytest
 
@@ -52,8 +53,8 @@ _TEST_LOCATION = PluginLocation(
 
 
 class _MockFetcherTrigger(PlainFetcherTrigger):
-    def __init__(self, payload: bytes) -> None:
-        super().__init__()
+    def __init__(self, payload: bytes, omd_root: Path) -> None:
+        super().__init__(omd_root)
         self._payload = payload
 
     def _trigger(self, fetcher: Fetcher, mode: Mode, secret: FetcherSecrets) -> result.Result:
@@ -108,7 +109,9 @@ class TestAutomationDiagHost:
                 edition=(app := make_app(edition(paths.omd_root))).edition,
                 make_bake_on_restart=app.make_bake_on_restart,
                 create_core=app.create_core,
-                make_fetcher_trigger=lambda *args: _MockFetcherTrigger(raw_data.encode("utf-8")),
+                make_fetcher_trigger=lambda *args: _MockFetcherTrigger(
+                    raw_data.encode("utf-8"), Path("/")
+                ),
                 make_metric_backend_fetcher=app.make_metric_backend_fetcher,
                 get_builtin_host_labels=app.get_builtin_host_labels,
             ),
