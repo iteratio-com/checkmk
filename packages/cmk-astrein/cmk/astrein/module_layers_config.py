c03a82f8547d5921a512070c82305b849a3cfcf8
Joerg Herbel <joerg.herbel@checkmk.com>
2026-03-04 21:50:14 +0100

    Revert "bakery merge type: extend and test legacy merge type list"
    This reverts commit aa112e6a8f3cda12d253c441286b38d11b154e83.
    
    Reason for revert: Breaks CI: https://ci.lan.tribe29.com/job/checkmk/job/master/job/light/job/test-plugins-siteless/3372
    
    Change-Id: I3b1e8af9f49e0586d3e669d1bf2e58a0525069bb

--- packages/cmk-astrein/cmk/astrein/module_layers_config.py
+++ packages/cmk-astrein/cmk/astrein/module_layers_config.py
@@ -1344,7 +1344,6 @@ COMPONENTS: Mapping[Component, ImportCheckerProtocol] = {
         "cmk.gui",
         "cmk.helper_interface",
         "cmk.logwatch.config",
-        "cmk.nonfree.pro.bakery",
         "cmk.piggyback",
         "cmk.server_side_calls_backend",
         "cmk.utils",
