7b10971e9f68e5ab7da5dd4a0ca0ecbadabdf73e
Lars Michelsen <lm@checkmk.com>
2026-03-07 00:01:58 +0100

    Move dcd GUI and protocols to package
    Change-Id: I015e37aa158d3c3613f51db2d5c87c405c92068d

--- omd/packages/omd/omdlib/__template_init__.py
+++ omd/packages/omd/omdlib/__template_init__.py
@@ -1,1 +1,8 @@
-__init__.py
+#!/usr/bin/env python3
+# Copyright (C) 2023 Checkmk GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+"""omd - Implementation of the OMD management program"""
+
+__version__ = "{CMK_VERSION}.{CMK_EDITION}"
