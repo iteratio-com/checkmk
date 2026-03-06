102b5eadd153e998a875f4065228c05a35acda9a
Leonardo Petrora <leonardo.petrora@checkmk.com>
2026-03-06 16:31:49 +0100

    dashboard: Update Add / edit view widget configuration
    When the "Specific object type" selector is disabled, then replace it
    with a label containing the selected option. This occurs when:
    * Copying a view
    * Linking to an exisiting view
    * Editing the widget
    
    CMK-31257
    
    Change-Id: I0faddc64784941a64d279c9748f7da56f4d1797c

--- packages/cmk-frontend-vue/src/dashboard/components/Wizard/wizards/view/stage1/components/ReferenceView.vue
+++ packages/cmk-frontend-vue/src/dashboard/components/Wizard/wizards/view/stage1/components/ReferenceView.vue
@@ -91,7 +91,6 @@ watch(
           v-model:context-infos="contextInfos"
           v-model:restricted-ids="restrictedToSingleInfos"
           v-model:mode="singleInfosMode"
-          :is-new-view="false"
           :read-only="true"
         />
       </div>
